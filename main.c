10aa42797aa656e8663c5fc22362528649464883
Dimoudis <98609381+Dimoudis@users.noreply.github.com>
2022-02-13 16:50:01 +0200

    Create the problem's structure

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+
+struct submission {
+    int cwNum;
+    char *title;
+    int firstRelativity;
+    int firstGrade;
+    int secondRelativity;
+    int secondGrade;
+    float finallyGrade;
+    int result;
+};
+
+
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+
+
+
 }
